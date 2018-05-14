e6b1b1fdbd70215332575469b76a67246e80f83f
ErkinKurt <kurt.erkin@gmail.com>
2018-05-15 00:21:38 +0300

    Testcommit

--- lib/Screens/MainScreen.dart
+++ lib/Screens/MainScreen.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:riot_application/Screens/HistoryScreen.dart';
-import 'package:riot_application/Screens/SummonerScreen.dart';
-import 'package:riot_application/Util/RiotApi.dart';
-import 'dart:async';
 
 class MainScreen extends StatefulWidget {
   @override
@@ -14,77 +11,26 @@ class MainScreen extends StatefulWidget {
 class MainScreenState extends State<MainScreen> {
   double width, height;
   String _summonerName;
-  RiotApi riotApi = new RiotApi();
 
-  onTextInputChanged(String value) {
-    setState(() {
+   onTextInputChanged(String value){
+    setState((){
       _summonerName = value;
       print(_summonerName);
     });
   }
 
-  onPressed() async {
-    await riotApi.setSummonerData(_summonerName);
-    await riotApi.getMatchListByAccountId();
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      child: new Dialog(
-        child: new Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            new CircularProgressIndicator(),
-            new Text("Loading"),
-          ],
-        ),
-      ),
-    );
-    new Future.delayed(new Duration(seconds: 3), () {
-      Navigator.pop(context); //pop dialog
-      Navigator.push(
-          context,
-          new MaterialPageRoute(
-              builder: (context) => new HistoryScreen(
-                  summonerName: _summonerName, riotApi: riotApi)));
-    });
-  }
-
-  onPressedSummoner() async {
-    await riotApi.setSummonerData(_summonerName);
-    await riotApi.getSummonerLeagueInfo();
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      child: new Dialog(
-        child: new Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            new CircularProgressIndicator(),
-            new Text("Loading"),
-          ],
-        ),
-      ),
-    );
-    new Future.delayed(new Duration(seconds: 1), () {
-      Navigator.pop(context); //pop dialog
-      Navigator.push(
-          context,
-          new MaterialPageRoute(
-              builder: (context) => new SummonerScreen(
-                  summonerName: _summonerName, riotApi: riotApi)));
-    });
+  onPressed(){
+    Navigator.push(context, new MaterialPageRoute(builder: (context) => new HistoryScreen(summonerName: _summonerName,)));
   }
 
   @override
   Widget build(BuildContext context) {
     width = MediaQuery.of(context).size.width;
     height = MediaQuery.of(context).size.height;
-    riotApi.getStaticChampionData();
+
     return new Container(
       //color: Colors.blue[900],
-      decoration: new BoxDecoration(
-          color: Colors.blue[900],
-          border: new Border.all(color: Colors.green, width: 10.0)),
+      decoration: new BoxDecoration(color: Colors.blue[900],border: new Border.all(color: Colors.green, width: 10.0)),
       child: new Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
@@ -104,8 +50,7 @@ class MainScreenState extends State<MainScreen> {
               new Buttons(
                 width: this.width,
                 height: this.height,
-                text: 'SUMMONER',
-                onPressed: this.onPressedSummoner,
+                text: 'LIVE!',
               )
             ],
           )
@@ -116,8 +61,7 @@ class MainScreenState extends State<MainScreen> {
 }
 
 class Buttons extends StatelessWidget {
-  Buttons(
-      {this.width, this.height, this.text, this.summonerName, this.onPressed});
+  Buttons({this.width, this.height, this.text, this.summonerName, this.onPressed});
 
   final double width;
   final double height;
@@ -134,15 +78,11 @@ class Buttons extends StatelessWidget {
       shape: new CircleBorder(
           side: new BorderSide(color: Colors.green, width: 5.0)),
       constraints: new BoxConstraints(
-          minWidth: this.width * 0.31, minHeight: this.height * 0.23),
+          minWidth: this.width * 0.31, minHeight: this.height * 0.20),
       child: new Text(
         text,
         style: new TextStyle(
-            color: Colors.white,
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            fontStyle: FontStyle.italic,
-            letterSpacing: 2.0),
+            color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold , fontStyle: FontStyle.italic, letterSpacing: 2.0 ),
       ),
     );
   }
@@ -165,11 +105,8 @@ class SummonerInput extends StatelessWidget {
       padding: new EdgeInsets.all(3.0),
       child: new TextField(
         onChanged: onChanged,
-        decoration: new InputDecoration(
-            contentPadding: new EdgeInsets.all(5.0),
-            hintText: "Enter your Summoner Name..."),
-        style: new TextStyle(
-            fontSize: 20.0, color: Colors.black, fontWeight: FontWeight.bold),
+        decoration: new InputDecoration(contentPadding: new EdgeInsets.all(5.0),hintText: "Enter your Summoner Name..."),
+        style: new TextStyle(fontSize: 20.0, color: Colors.black, fontWeight: FontWeight.bold),
       ),
     );
   }
